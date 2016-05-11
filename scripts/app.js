ea3515ebc9ee0f604a764185e6128e25ffc24990
Ben Hulan <benhulan@gmail.com>
2016-05-11 15:09:02 -0700

    adds in-class work

--- scripts/app.js
+++ scripts/app.js
@@ -9,24 +9,19 @@ $(document).on("ready", function() {
     $quakesList = $.ajax({
         method: "GET",
         url: weekly_quakes_endpoint
-    });
-
-    $quakesList.done(function(data){
+    })
+.done(function(data){
 
        var earthquakes = data.features;
 
-        // var source = $("#quakes-template").html();
-        // template = Handlebars.compile(source);
+        var source = $("#quakes-template").html();
+        template = Handlebars.compile(source);
 
-        // var quakesTemplate = template({quakes: earthquakes});
-        for (var i = 0; i < earthquakes.length; i++){
-        var quakesTemplate = "<p>" + earthquakes[i].properties.title + "</p>" +
-        "<h3>LATITUDE: " + earthquakes[i].geometry.coordinates[0] + "</h3>";
+        var quakesTemplate = template({quakes: earthquakes});
+        
         $("#info").append(quakesTemplate);
-        }
-    });
-
-    $quakesList.fail(function(response){
+    })
+    .fail(function(response){
         console.log("Error: ", response);
     });
 });
