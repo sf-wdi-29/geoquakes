c1e46cf4519be8d91057adb5d295738ed5623db1
Kenneth Bushman <k.bushman@yahoo.com>
2016-09-15 08:58:47 -0700

    adds dynamic map markers

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,9 @@ var rawEarthquakeData;
 var map;
 
 function createGoogleMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 37.78, lng: -122.44},
-      zoom: 8
+      zoom: 2
     });
 
   var latLng = new google.maps.LatLng(37.78,-122.44);
@@ -16,6 +16,7 @@ function createGoogleMap() {
   });
 
 }
+
 
 $(document).on("ready", function() {
 
@@ -38,9 +39,11 @@ $(document).on("ready", function() {
       $("#info").append(cleanEarthquakeData);
 
       //create map coordinates and markers
+      console.log("i am here!")
       selectFeatures.forEach(function(location) {
-        var latLng = new google.maps.LatLng(location.geometry.coordinates[1], location.geometry.coordinates[0]);
+        var latLng = {lat: location.geometry.coordinates[1], lng: location.geometry.coordinates[0]};
 //        var lng = location.geometry.coordinates[0];
+        console.log(latLng);
         var marker = new google.maps.Marker({
           position: latLng,
           map: map
@@ -53,4 +56,7 @@ $(document).on("ready", function() {
   });
 
 
+
 });
+
+
