b748e8303e6f68cb2079ad133f60c160b53f363e
brikky <brandon.kerr160@topper.wku.edu>
2016-09-14 17:40:03 -0700

    finishes geoquakes lab

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,47 @@
 // define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var earthquakeApiUrl = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 37.78,
+            lng: -122.44
+        },
+        zoom: 2
+    });
+}
 
 $(document).on("ready", function() {
 
-  // CODE IN HERE!
+    initMap();
 
-});
+    $.ajax({
+        type: "GET",
+        url: earthquakeApiUrl,
+        dataType: 'json',
+        success: function(data) {
+            var earthquakes = data.features;
+            var source = $("#earthquake-title-template").html();
+            var template = Handlebars.compile(source);
+            var earthquakeHtml = template({
+                earthquakes: earthquakes
+            });
+            $("#info").append(earthquakeHtml);
+
+            earthquakes.forEach(function(earthquake) {
+                var earthquakeLatLng = {
+                    lng: earthquake.geometry.coordinates[0],
+                    lat: earthquake.geometry.coordinates[1]
+                };
+                var marker = new google.maps.Marker({
+                    position: earthquakeLatLng,
+                    map: map
+                });
+            });
+
+        }
+
+        // CODE IN HERE!
+    });
+})
